d9cc87bd591081e077d093c8e619c1387bff0f3a
AndrewByrne17 <andrewabyrne17@gmail.com>
2025-05-14 10:27:59 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,1 @@
  print("Hello from Binder!")
-
-echo "# my-first-binder" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/AndrewByrne17/my-first-binder.git
-git push -u origin main
-
